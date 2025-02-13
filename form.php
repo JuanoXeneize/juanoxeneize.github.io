2e93e210c4b5ce1c8e8eb446499e9ca7c68addd6
JuanoXeneize <69049672+JuanoXeneize@users.noreply.github.com>
2025-02-13 12:45:39 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,39 @@
+
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+$mail = new PHPMailer(true);
+
+try {
+    // Configuración del servidor SMTP
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'mail.apolofish.com'; 
+    $mail->SMTPAuth = true;
+    $mail->Username = 'info@apolofish.com';
+    $mail->Password = 'MCjKoNsDlqN6'; 
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 587;
+
+    // Datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+
+    // Configuración del email
+    $mail->setFrom($email, $nombre);
+    $mail->addAddress('info@apolofish.com');
+    $mail->Subject = "Mensaje vía web de $nombre";
+    $mail->Body = "Nombre: $nombre\nEmail: $email\nMensaje:\n$mensaje";
+
+    $mail->send();
+    header('Location: gracias.htm');
+} catch (Exception $e) {
+    echo "Error al enviar el correo: {$mail->ErrorInfo}";
+}
+?>
